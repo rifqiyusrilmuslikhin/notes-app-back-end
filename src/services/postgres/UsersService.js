cca139f9960c398e51461442b7937cde3cca5b77
rifqiyusrilmuslikhin <ryusril067@gmail.com>
2022-03-30 17:43:24 +0700

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,7 +3,8 @@ const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
- 
+const AuthenticationError = require('../../exceptions/AuthenticationError');
+
 class UsersService {
   constructor() {
     this._pool = new Pool();
@@ -11,21 +12,18 @@ class UsersService {
 
   async addUser({ username, password, fullname }) {
     await this.verifyNewUsername(username);
-
     const id = `user-${nanoid(16)}`;
     const hashedPassword = await bcrypt.hash(password, 10);
- 
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
-      values: [id, username, password, fullname],
+      values: [id, username, hashedPassword, fullname],
     };
- 
+
     const result = await this._pool.query(query);
- 
-    if (!result.rows[0].id) {
-      throw new InvariantError('Catatan gagal ditambahkan');
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
     }
- 
     return result.rows[0].id;
   }
 
@@ -34,11 +32,11 @@ class UsersService {
       text: 'SELECT username FROM users WHERE username = $1',
       values: [username],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.')
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
 
@@ -47,17 +45,38 @@ class UsersService {
       text: 'SELECT id, username, fullname FROM users WHERE id = $1',
       values: [userId],
     };
- 
+
     const result = await this._pool.query(query);
-    
+
     if (!result.rows.length) {
       throw new NotFoundError('User tidak ditemukan');
     }
- 
+
     return result.rows[0];
   }
 
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
 
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
+  }
 }
 
 module.exports = UsersService;
